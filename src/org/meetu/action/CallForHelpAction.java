8a70610811657d53fc7d5ad16a41bcb67d35f2f3
dirkmurphy <giuge@aliyun.com>
2015-08-08 16:55:13 +0800

    bootstrap前端页面

---
+++ src/org/meetu/action/CallForHelpAction.java
@@ -0,0 +1,20 @@
+package org.meetu.action;
+
+/**
+ * 主动求救Action
+ * */
+public class CallForHelpAction {
+
+	/**
+	 * 发起求救,客户端应做成一键求救/手势求救<br>
+	 * 0.入库<br>
+	 * 1.发短信<br>
+	 * 2.发推送<br>
+	 * 3.发邮件
+	 * */
+	public String callForHelp() {
+		
+		return null;
+	}
+	
+}
