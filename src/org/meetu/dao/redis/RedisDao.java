03858e2e7e52f98a591483d64a0862efbc28fadb
dirkmurphy <giuge@aliyun.com>
2015-10-21 17:10:47 +0800

    使用jedis测试redis可用性

--- src/org/meetu/dao/redis/RedisDao.java
+++ src/org/meetu/dao/redis/RedisDao.java
@@ -1,5 +1,29 @@
 package org.meetu.dao.redis;
 
-public class RedisDao {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
+public class RedisDao {
+	
+	public void insert() {
+		
+	}
+	
+	public void update() {
+		
+	}
+	
+	public void delete() {
+		
+	}
+	
+	public Map selectAll() {
+		return null;
+	}
+	
+	public List selectByKey(String key) {
+		return null;
+	}
+	
 }
