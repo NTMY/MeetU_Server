c35a262f7cc4951ad0d2081a74ed3ad7cbee2b1f
dirkmurphy <giuge@aliyun.com>
2015-10-22 17:27:18 +0800

    nothing

--- src/org/meetu/util/RangeCalculator.java
+++ src/org/meetu/util/RangeCalculator.java
@@ -7,11 +7,12 @@ import java.util.Arrays;
  * */
 public class RangeCalculator {
 
-	public static void main(String[] args) {
-		double[] around = getSquare(50.000000,100.000000,1000);
-		System.out.println(Arrays.toString(around));
-	}
-
+    private static final double PI = 3.14159265;  
+    private static final double EARTH_RADIUS = 6378137;  
+    private static final double RAD = Math.PI / 180.0;  
+	
+     
+    //24,901/360 = 69.17 miles / degree  
 	/**
 	 * 生成以中心点为中心的四方形经纬度
 	 * @param lat 纬度
@@ -23,7 +24,7 @@ public class RangeCalculator {
 
 		Double latitude = lat;
 		Double longitude = lon;
-
+		//The circumference of the earth is 24,901 miles. 
 		Double degree = (24901 * 1609) / 360.0;
 		double raidusMile = raidus;
 
@@ -40,4 +41,33 @@ public class RangeCalculator {
 		return new double[] { minLat, minLng, maxLat, maxLng };
 	}
 
+	
+	
+    /** 
+     * 根据两点间经纬度坐标（double值），计算两点间距离，单位为米 
+     * @param lng1 
+     * @param lat1 
+     * @param lng2 
+     * @param lat2 
+     * @return 
+     */  
+    public static double getDistance(double lng1, double lat1, double lng2, double lat2)  
+    {  
+       double radLat1 = lat1*RAD;  
+       double radLat2 = lat2*RAD;  
+       double a = radLat1 - radLat2;  
+       double b = (lng1 - lng2)*RAD;  
+       double s = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a/2),2) +  
+        Math.cos(radLat1)*Math.cos(radLat2)*Math.pow(Math.sin(b/2),2)));  
+       s = s * EARTH_RADIUS;  
+       s = Math.round(s * 10000) / 10000;  
+       return s;  
+    }  
+	
+    
+	public static void main(String[] args) {
+		double[] around = getSquare(50.000000,100.000000,1000);
+		System.out.println(Arrays.toString(around));
+	}
+    
 }
